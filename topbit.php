69a5db2fc5258755f408de6d3f8fcf1cdeb3cb45
FriedSnakes <126204563+FriedSnakes@users.noreply.github.com>
2023-10-25 09:39:41 +1300

    Create topbit.php

---
+++ topbit.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="home.php">
+                <img src="images/logo.png" alt="Your Button" width="200" height="150">
+				<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+	<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+</head>
+            </a>
+        </div>
+        <div class="nav-links">
+            <a href="home.php">Home</a><br>
+            <a href="reviewsdisplay.php">Reviews</a><br>
+            <a href="insertform.php">Write a review</a><br>
+            <a href="aboutus.php">About Us</a>
+        </div>
+    </nav>
+</header>
