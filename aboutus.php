2cb3450be84161bd00874a9ad631de6a64abf59e
FriedSnakes <126204563+FriedSnakes@users.noreply.github.com>
2023-10-25 09:36:04 +1300

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,34 @@
+<?php include("topbit.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About Us</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<main id="content">
+	<h1>Our Story</h1>
+	</main>
+	<main id="content">
+	
+
+	
+	<p>Tutoko Delights has begun as a small local business for healthy alternatives to tasty treats here in Invercargill. We work with the Seriously Good Chocolate Company to produce our own healthy chocolate bars, and sell them for the best price we can.</p>
+
+<p>Tutoko Delights started thanks to a Year 12 Business Studies Program. The Young Enterprise Scheme from The Lion Foundation helped us to meet goals and complete challenges which assisted us in growing our business.</p>
+
+<p>We decided to create healthy treats because we realised that there aren’t many tasty, yet healthier alternatives to most delightful confectionary. We decided to experiment by mixing some chocolate with a multivitamin powder. We were surprised when it turned out to taste great on the first try. We then continued experimenting with different flavours, and today we are able to offer three flavours of vitamin-infused 60g chocolate bars. We hope you enjoy our products!</p>
+		
+		<h3>Our team</h3>
+		<ul>
+  		<li>CEO - Matthew Smith</li>
+  		<li>Marketing Director - Christian Henderson</li>
+  		<li>Finance Director - Carlos McRae</li>
+		<li>Human Resources - Veresa Rakaidawa</li>
+		<li>Production Director - Ranel Colvin</li>
+		</ul>
+	</main>
+</body>
+</html>
